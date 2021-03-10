8ab59cff84e0da33329b25f459192a89a0ed1386
Ju Long <19013345@myrp.edu.sg>
2021-03-10 10:03:21 +0800

    update on the app.blade.php

--- resources/views/apps.blade.php
+++ resources/views/apps.blade.php
@@ -11,17 +11,19 @@
     <section id="airport">
         <div class="app-title">
             <strong>Airport Navigation</strong>
+            <button type="button" class="btn">show me</button>
         </div>
         <a class="app-content" href="/airport">
-            <img src="../img/movie/CoverPage.svg" alt="">
+            <img src="../img/airport/CoverPage.svg" class="app-img">
         </a>
     </section>
     <section id="gymlog">
         <div class="app-title">
             <strong>Gym Log</strong>
+            <button type="button" class="btn">show me</button>
         </div>
         <a class="app-content" href="/gymlog">
-            
+
         </a>
     </section>
 </div>
